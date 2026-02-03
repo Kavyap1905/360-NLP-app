6bcdf0f62a5adfe4a8c679fe46b2bb33bc79781c
Kavyap1905 <118955083+Kavyap1905@users.noreply.github.com>
2026-02-04 03:40:22 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -100,6 +100,43 @@ def speech_sentiment():
     except Exception as e:
         return "Could not understand audio"
 
+def camera_emotion_detection():
+    detector = FER(mtcnn=True)
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        result = detector.detect_emotions(frame)
+
+        for face in result:
+            (x, y, w, h) = face["box"]
+            emotions = face["emotions"]
+            dominant_emotion = max(emotions, key=emotions.get)
+
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 2)
+            cv2.putText(
+                frame,
+                dominant_emotion,
+                (x, y-10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.9,
+                (0,255,0),
+                2
+            )
+
+        cv2.imshow("Live Emotion Detection (Press Q to quit)", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
 def read_document(uploaded_file):
     if uploaded_file is None:
         return ""
@@ -123,4 +160,5 @@ def read_document(uploaded_file):
 
     else:
         raise ValueError("Unsupported file type")
+
 
